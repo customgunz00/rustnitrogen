a66b122b47ab30ab3b41a7a5aed7ee5a130f0ec6
customgunz00 <159488157+customgunz00@users.noreply.github.com>
2024-04-17 12:09:01 -0600

    clean

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,23 @@ use reqwest::blocking::Client;
 use rand::distributions::{Alphanumeric, Distribution, Uniform};
 use rand::{thread_rng, Rng};
 
-fn main() -> Result<(), reqwest::Error> {
-    let client = Client::new();
+use std::fs::File;
+use std::io::Write; 
 
-    fn generate_random_string(length: usize) -> String {
-        thread_rng()
-            .sample_iter(&Alphanumeric)
-            .take(length)
-            .map(char::from)
-            .collect()
-    }
+fn generate_random_string(length: usize) -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(length)
+        .map(char::from)
+        .collect()
+}
 
-    fn generate_random_number(min: i32, max: i32) -> i32 {
-        let num: i32 = thread_rng().gen_range(min..max);
-        return num;
-    }
+fn generate_random_number(min: i32, max: i32) -> i32 {
+    let num: i32 = thread_rng().gen_range(min..max);
+    return num;
+}
 
+fn makereq(cli: Client) -> Result<(), reqwest::Error> {
     let rn = generate_random_number(13, 24);
     let rs = generate_random_string(rn as usize);
 
@@ -25,10 +26,22 @@ fn main() -> Result<(), reqwest::Error> {
 
     let finished_url = format!("{}{}", url, rs);
 
-    let response = client.get(finished_url).send()?;
+    let response = cli.get(finished_url).send()?;
 
-    println!("Status: {}", response.status());
-    println!("Code: {}", rs);
-
+    if response.status() == reqwest::StatusCode::NOT_FOUND {
+        println!("{} | CODE INVALID", rs);
+    } else if response.status() == reqwest::StatusCode::TOO_MANY_REQUESTS {
+        println!("Waiting for 7.5 seconds... | RATE LIMITED");
+    } else if response.status() == reqwest::StatusCode::OK {
+        println!("{} | CODE VALID", rs);
+    }
+    
     Ok(())
+}
+
+
+fn main() {
+    let client: Client = Client::new();
+
+    makereq(client);
 }
