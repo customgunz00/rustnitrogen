dd312be725d476447ad14c28e90bba5cc02703ca
customgunz00 <159488157+customgunz00@users.noreply.github.com>
2024-04-17 10:59:40 -0600

    random & requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::blocking::Client;
+use rand::distributions::{Alphanumeric, Distribution, Uniform};
+use rand::{thread_rng, Rng};
+
+fn main() -> Result<(), reqwest::Error> {
+    let client = Client::new();
+    let mut response = client.get("https://example.com").send()?;
+
+    fn generate_random_string(length: usize) -> String {
+        thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(length)
+            .map(char::from)
+            .collect()
+    }
+
+    fn generate_random_number(min: i32, max: i32) -> i32 {
+        let num: i32 = thread_rng().gen_range(min..max);
+        return num;
+    }
+
+    let rn = generate_random_number(1, 100);
+    let rs = generate_random_string(rn as usize);
+
+    println!("{}", rn);
+    println!("{}", rs);
+
+    Ok(())
 }
